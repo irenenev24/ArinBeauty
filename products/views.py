fb2c6c181991e5674e8a534985dddd0d39920115
Inev367 <irenenev24@gmail.com>
2021-12-17 17:55:40 +0000

    Added products.html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ Aview to show all products, including sorting and queries """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
