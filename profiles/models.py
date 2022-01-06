a03b09833697ba26c40259131bb162d5ad027419
Inev367 <irenenev24@gmail.com>
2022-01-06 16:25:04 +0000

    Added allauth form details and css

--- profiles/models.py
+++ profiles/models.py
@@ -24,7 +24,7 @@ class UserProfile(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     ### Create or update the user profile ###
-    if created:
+    # if created:
         UserProfile/objects.create(user=instance)
-    #Existing users: just need to save the profile
-    instance.userprofile.save()
+    # Existing users: just need to save the profile
+    # instance.userprofile.save()
