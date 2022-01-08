911ca0c75cb4b867fc50fd3c36b6817f5ff45721
Inev367 <irenenev24@gmail.com>
2022-01-08 15:12:47 +0000

    Added more Stripe elements. Views and urls for cached_data

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -23,7 +23,7 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
+            payload, sig_header, wh_secret
         )
     except ValueError as e:
         # Invalid payload
@@ -37,20 +37,17 @@ def webhook(request):
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
-    # Map webhook events to relevant handler functions
+    # Map webhook events to relevane functions
     event_map = {
-        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+        'payment_intent.succeeded': handler.handle_payment_intent_suceeded,
+        'payment_intent.failed': handler.handle_payment_intent_payment_failed,
     }
 
     # Get the webhook type from Stripe
     event_type = event['type']
 
-    # If there's a handler for it, get it from the event map
-    # Use the generic one by default
-    event_handler = event_map.get(event_type, handler.handle_event)
+    event_handler = event_map.get(event_type)
 
     # Call the event handler with the event
     response = event_handler(event)
     return response
-
