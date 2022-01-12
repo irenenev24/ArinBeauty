d32eb71b2ddd4a97758cca6b10fc2732422ce119
Inev367 <irenenev24@gmail.com>
2022-01-12 17:05:58 +0000

    Added edit_product.html view and url to allow admin to edit products

--- products/views.py
+++ products/views.py
@@ -75,16 +75,40 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully added product.')
+            messages.success(request, f'Successfully added product: {product.name}.')
             return redirect(reverse('add_product'))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, f'Failed to add product: {product.name}. Please ensure the form is valid.')
     else:
         form = ProductForm()
     
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    ### Edit a product in store ###
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Successfully updated product: {product.name}!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Failed to update product: {product.name}. Please ensure form is valid and try again.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing product: {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
