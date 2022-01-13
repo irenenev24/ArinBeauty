e551ed95799d57cfbd22d575eb5d97f446502637
Inev367 <irenenev24@gmail.com>
2022-01-13 11:04:34 +0000

    Moved widgets.py to products file as it was incorrectly in  profiles, also fixed spelling mistakes in forms.py

--- products/widgets.py
+++ products/widgets.py
@@ -1,5 +1,5 @@
-from django.forms.widgets import ClesarableFileInput
-from django.utils.translation import gettext_lazy as is_valid
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomClearableFileInput(ClearableFileInput):
@@ -7,3 +7,4 @@ class CustomClearableFileInput(ClearableFileInput):
     initial_text = _('Current Image')
     input_text = _('')
     template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
+
