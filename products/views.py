64386a468d22161208c920b581cf87a188413430
Inev367 <irenenev24@gmail.com>
2022-01-13 15:09:42 +0000

    Made edits to categories.json and products.json and reloaded data to help select different categories of product

--- products/views.py
+++ products/views.py
@@ -73,22 +73,22 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-    ### Add a product to the store ###
+    """ Add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, this service is only for store owners')
+        messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
-            messages.success(request, f'Successfully added product: {product.name}.')
-            redirect(reverse('product_detail', args=[product.id]))
+            messages.success(request, f'Successfully added {product.name}!')
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, f'Failed to add product: {product.name}. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = ProductForm()
-    
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
