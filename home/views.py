6fade734444c6dc4fb3bfccab8f186df198446a2
Inev367 <irenenev24@gmail.com>
2022-01-13 22:06:17 +0000

    Added a new index.html to act as a cover page for the site. edited some css to adjust image on store.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 # Create your views here.
+
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def store(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/store.html')
