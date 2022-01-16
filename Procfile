7c0dc0d4b7c4283dd02e6d9360e4fa7effcee270
Inev367 <irenenev24@gmail.com>
2022-01-16 11:41:21 +0000

    Deployment step two

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn arin_beauty.wsgi.application
--- arin_beauty/settings.py
+++ arin_beauty/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    'arin_beauty.herokuapp.com', 'localhost'
 ]
 
 
@@ -122,13 +123,17 @@ WSGI_APPLICATION = 'arin_beauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
