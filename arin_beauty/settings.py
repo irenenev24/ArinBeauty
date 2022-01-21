7ba78c554bb9254af2bc29c8fd8c77f0efa10165
Inev367 <irenenev24@gmail.com>
2022-01-21 18:30:42 +0000

    Edited aws bucket variable in settings.py

--- arin_beauty/settings.py
+++ arin_beauty/settings.py
@@ -187,7 +187,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'arin-beauty-products'
+    AWS_STORAGE_BUCKET_NAME = 'arin-beauty'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
