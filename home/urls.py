723820f00ed2aea40a9a1dfeb6312431ba815944
Inev367 <irenenev24@gmail.com>
2022-01-21 18:49:29 +0000

    Edited url for index.html

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
     path('store/', views.store, name='store'),
 ]
