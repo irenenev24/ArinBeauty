6c5c20764ef766700253972ac90002b48f95118a
Inev367 <irenenev24@gmail.com>
2022-01-22 15:47:22 +0000

    Started blog app

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from .models import Category, Post
+
+# Create your views here.
+def blog(request):
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'blog/blog.html', context)
--- products/models.py
+++ products/models.py
@@ -30,6 +30,3 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
-    
-    
